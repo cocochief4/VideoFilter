a1cba44c3859f72905307f9080886b8378023092
Cocochief <cocochief4@gmail.com>
2024-01-22 23:38:20 -0800

    kmeans!

---
+++ src/Filters/KMeansFilter.java
@@ -0,0 +1,13 @@
+package Filters;
+
+import Interfaces.PixelFilter;
+import core.DImage;
+
+public class KMeansFilter implements PixelFilter{
+    @Override
+    public DImage processImage(DImage img) {
+        
+
+        
+    }
+}
