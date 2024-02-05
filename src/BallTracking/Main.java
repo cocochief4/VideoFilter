12b962dc76ed73e9e2987da63c5864243118f81a
Cocochief <cocochief4@gmail.com>
2024-02-05 10:59:41 -0800

    start balltracking

---
+++ src/BallTracking/Main.java
@@ -0,0 +1,13 @@
+package BallTracking;
+
+import Interfaces.PixelFilter;
+import core.DImage;
+
+public class Main implements PixelFilter {
+     
+    @Override
+    public DImage processImage(DImage img) {
+
+    }
+}
+ 
