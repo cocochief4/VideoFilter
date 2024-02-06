969edea5da0d0ca30062657df91433b813d2ec7c
Cocochief <cocochief4@gmail.com>
2024-02-06 12:07:09 -0800

    create overlay

--- src/BallTracking/Main.java
+++ src/BallTracking/Main.java
@@ -5,9 +5,7 @@ import core.DImage;
 
 import java.util.ArrayList;
 
-import BallTracking.Blur.*;
-import BallTracking.CenterDrawer.*;
-import BallTracking.Threshold.*;
+import BallTracking.*;
 
 public class Main implements PixelFilter {
      
@@ -15,7 +13,7 @@ public class Main implements PixelFilter {
     public DImage processImage(DImage img) {
 
         DImage original = new DImage(img);
-        original.setColorChannels(img.getRedChannel(), img.getGreenChannel(), img.getBlueChannel());
+        original.setColorChannels(img.getRedChannel().clone(), img.getGreenChannel().clone(), img.getBlueChannel().clone());
 
         ArrayList<PixelFilter> pipeline = new ArrayList<PixelFilter>();
         pipeline.add(new Blur());
@@ -25,6 +23,10 @@ public class Main implements PixelFilter {
         for (int i = 0; i < pipeline.size(); i++) {
             pipeline.get(i).processImage(img);
         }
+        
+        PixelFilter overlay = new Overlay();
+        
+        img = overlay.overlay(original, img);
 
         return img;
     }
